3545b3c3463f1d3f6c8f56dc8470e295cab57538
Natasha England-Elbro <ashenglandelbro@protonmail.com>
2022-03-20 17:56:34 +0000

    feat: add arg parsing and inplace update

--- app/Main.hs
+++ app/Main.hs
@@ -1,17 +1,46 @@
+{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TupleSections #-}
 module Main where
 
-import Lib
+import qualified Lib
 import qualified Types as T
 import qualified System.Environment as SE
-import Data.Text (pack, unpack)
+import Data.Text (Text, pack, unpack)
 import Data.Bifunctor (Bifunctor(bimap))
 import qualified System.IO as IO
+import qualified Args as ARG
+import Data.List (find)
+import Data.Maybe (isJust)
+import qualified Data.Text as TxT
+import qualified Data.Text.IO as TxT
+
 
 main :: IO ()
-main = do
-    args <- SE.getArgs
-    content <- readFile (head args)
-    commented <- bimap unpack unpack <$> run (T.SourceInfo (pack (head args)) (pack content))
-    IO.hPutStrLn IO.stdout $ fst commented
-    IO.hPutStrLn IO.stderr $ snd commented
+main = ARG.exec >>= handleArgs
+
+run :: Text -> IO (Text, Text)
+run name = do
+    content <- readInput name
+    Lib.run (T.SourceInfo name content)
+
+readInput :: Text -> IO Text
+readInput "-" = TxT.hGetContents IO.stdin
+readInput name = TxT.readFile (TxT.unpack name)
+
+
+writeOutput :: IO.Handle -> (Text, Text) -> IO ()
+writeOutput h (content, "") = TxT.hPutStr h content
+writeOutput _ (_, errs) = TxT.hPutStr IO.stderr errs
+
+handleArgs args
+    | isJust (find (== ARG.FlagInplace) (ARG.appFlags args)) = do
+            file <- inputFile
+            rs <- result
+            writeOutput file rs
+            IO.hClose file
+    | otherwise = do
+        rs <- result
+        writeOutput IO.stdout rs
+    where
+        result = run $ ARG.appInput args
+        inputFile = IO.openFile (TxT.unpack $ ARG.appInput args) IO.ReadMode
---
+++ src/Args.hs
@@ -0,0 +1,39 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Args (
+    exec
+  , AppFlag(..)
+  , AppOpts(..)
+)
+where
+
+import qualified Options.Applicative as ARG
+import Options.Applicative (flag', long, short, help, strArgument, metavar, value)
+import Control.Applicative ((<**>), many)
+import Data.Text (Text)
+import qualified Data.Text as TxT
+
+
+data AppFlag = FlagInplace deriving(Eq)
+data AppOpts = AppOpts { appInput :: !Text, appFlags :: ![AppFlag] } deriving (Eq)
+
+parseAppFlag :: ARG.Parser AppFlag
+parseAppFlag = flag' FlagInplace (short 'i' <> help "modify in place")
+
+txtArgument :: ARG.Mod ARG.ArgumentFields String -> ARG.Parser Text
+txtArgument = fmap TxT.pack . strArgument
+
+parseAppOpts :: ARG.Parser AppOpts
+parseAppOpts = AppOpts <$>
+    txtArgument (
+        metavar "INPUT"
+        <> help "Input file or - for stdin"
+        <> value "-"
+    )
+    <*> many parseAppFlag
+
+exec :: IO AppOpts
+exec = ARG.execParser $ ARG.info (parseAppOpts <**> ARG.helper) (ARG.header "marked-imports")
+
+
+
+
