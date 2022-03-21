b48b924097b12391a35f3e4294fd751b27b28201
Natasha England-Elbro <ashenglandelbro@protonmail.com>
2022-03-21 00:25:40 +0000

    chore: add more tests

--- test/TestLib.hs
+++ test/TestLib.hs
@@ -14,16 +14,6 @@ import Test.Hspec
 
 import Test.QuickCheck as QC
 
-newtype RunResults = RunResults (Text -> IO (Text, Text))
-newtype RunInput = RunInput Text
-
-instance QC.Arbitrary RunResults where
-    arbitrary = foldl1 (\(RunResults last) (RunResults x) -> RunResults (last >=> \(out, _) -> x out)) <$> QC.listOf1 (pure $ RunResults (L.run . testSrc))
-
-instance QC.Arbitrary RunInput where
-    arbitrary = fmap RunInput $ (pure "module T where\nimport ") ><> ((Util.mconcatInfix "\nimport " . map T.modName) <$> QC.listOf (arbitrary :: Gen T.ModuleName))
-instance QC.Arbitrary T.ModuleName where
-    arbitrary = T.ModuleName . TxT.pack <$> (QC.suchThat (arbitrary :: Gen String) (\s -> s != [] && all (\x -> isUpper x || isDigit x) s))
 
 exampleCommentedInput = "module T where\n-- text\nimport Data.Text\n"
 testSrc = T.SourceInfo ""
@@ -34,8 +24,11 @@ spec = context "lib tests" $ do
         rs <- L.runWithCtx (TUtil.mkDummyCtx "text") (testSrc exampleCommentedInput)
         rs `shouldOutputOk` exampleCommentedInput
     it "produces same output from run to run" $ do
-        modules <- QC.generate $ QC.listOf1 (pure "-- text\nData.Text\n")
-        let doc = "module T where\nimport " <> Util.mconcatInfix "\nimport " modules
-        rs <- L.runWithCtx (TUtil.mkDummyCtx "text") (testSrc doc)
-        rs `shouldOutputOk` doc
+        let modules = replicate 5 "Data.Text" <> replicate 3 "Data.Maybe"
+        let doc = "module T where\n-- text\nimport " <> Util.mconcatInfix "\nimport " modules <> "\n"
+        let runner doc = L.runWithCtx (TUtil.mkDummyCtx "text") (testSrc doc)
+        rs <- runner doc
+        rs' <- runner (fst rs)
+        rs'' <- runner (fst rs')
+        rs'' `shouldOutputOk` doc
 
