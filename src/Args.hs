a57037f7019804e6ee29bbf215194647bfac888d
Natasha England-Elbro <ashenglandelbro@protonmail.com>
2022-03-28 19:39:33 +0100

    chore: save

--- src/Args.hs
+++ src/Args.hs
@@ -7,17 +7,17 @@ module Args (
 where
 
 import qualified Options.Applicative as ARG
-import Options.Applicative (flag', long, short, help, strArgument, metavar, value)
+import Options.Applicative (flag', long, short, help, strArgument, metavar, value, (<|>))
 import Control.Applicative ((<**>), many)
 import Data.Text (Text)
 import qualified Data.Text as TxT
 
 
-data AppFlag = FlagInplace deriving(Eq)
+data AppFlag = FlagInplace | FlagStripComments deriving(Eq)
 data AppOpts = AppOpts { appInput :: !Text, appFlags :: ![AppFlag] } deriving (Eq)
 
 parseAppFlag :: ARG.Parser AppFlag
-parseAppFlag = flag' FlagInplace (short 'i' <> help "modify in place")
+parseAppFlag = flag' FlagInplace (short 'i' <> help "modify in place") <|> flag' FlagStripComments (short 's' <> help "strip comments added by this tool" <> long "strip")
 
 txtArgument :: ARG.Mod ARG.ArgumentFields String -> ARG.Parser Text
 txtArgument = fmap TxT.pack . strArgument
